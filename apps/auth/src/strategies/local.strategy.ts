e27c8aa292b19aa05b92bf21e7f05fecb01624d2
harry log <harry8log@gmail.com>
2024-12-02 12:03:40 +0200

    update

---
+++ apps/auth/src/strategies/local.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class LocalStategy extends PassportStrategy(Strategy) {
+  constructor(private readonly usersService: UsersService) {
+    super({ usernameField: 'email' });
+  }
+
+  async validate(email: string, password: string) {
+    try {
+      return await this.usersService.verifyUser(email, password);
+    } catch (err) {
+      throw new UnauthorizedException(err);
+    }
+  }
+}
+
+/*In NestJS, once you've defined an authentication strategy and
+ registered it as a provider in a module, you can access the user 
+ object or user information that the strategy returns in your application's 
+ request object using the @Request() decorator.
+ */
+
+//related to : export class LocalAuthGuard extends AuthGuard('local') {}
+// and   @UseGuards(LocalAuthGuard)
