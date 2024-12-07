5cefc78df60f796a9cb0d5ca6e16f082cce6badb
harry <harry8log@gmail.com>
2024-12-07 16:13:34 +0200

    update

--- ng-stamina/src/app/auth/auth.service.ts
+++ ng-stamina/src/app/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { BehaviorSubject, Observable, tap, map } from 'rxjs';
 import { environment } from '../../environments';
 import { User } from '../users/models/user.model';
-
+interface LoginResponse {
+  access_token: string;
+  user: User;
+  expiresIn?: number;
+}
 @Injectable({
   providedIn: 'root',
 })
@@ -14,10 +18,24 @@ export class AuthService {
   private apiUrl = `${environment.apiUrl}/auth`;
 
   login(email: string, password: string): Observable<User> {
-    return this.http.post<User>(`${this.apiUrl}/login`, {
-      email,
-      password,
-    });
+    return this.http
+      .post<LoginResponse>(
+        `${this.apiUrl}/login`,
+        {
+          email,
+          password,
+        },
+        { withCredentials: true }
+      )
+      .pipe(
+        tap((response: LoginResponse) => {
+          // Store the token
+          localStorage.setItem('auth_token', response.access_token);
+          // Update the user subject
+          this.user$.next(response.user);
+        }),
+        map((response: LoginResponse) => response.user) // Return just the user for the store
+      );
   }
 
   signup(email: string, password: string, name?: string): Observable<User> {
