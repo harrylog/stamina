eb5fe40974670d98634ef2c72d191a3bd1d7a97a
harry log <harry8log@gmail.com>
2024-12-16 13:03:24 +0200

    update

--- ng-stamina/src/app/users/components/user-details/user-details.component.ts
+++ ng-stamina/src/app/users/components/user-details/user-details.component.ts
@@ -1,23 +1,50 @@
-import { Component, inject } from '@angular/core';
-import { selectQueryParam, selectRouteParam } from '../../../router.selectors';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { Router, RouterModule } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { selectCurrentUser } from '../../store/user.selectors';
+import { Observable, switchMap, take } from 'rxjs';
+import { selectRouteParam } from '../../../router.selectors';
+import { UserActions } from '../../store/user.actions';
+import { User } from '../../models/user.model';
+import { selectCurrentUser, selectSelectedUserId } from '../../store/user.selectors';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    RouterModule,
+  ],
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.scss',
 })
-export class UserDetailsComponent {
+export class UserDetailsComponent implements OnInit {
   private store = inject(Store);
+  private router = inject(Router);
 
-  // Get route parameters
   userId$ = this.store.select(selectRouteParam('id'));
-
-  // Get query parameters
-  sortBy$ = this.store.select(selectQueryParam('sort'));
-
   currentUser$ = this.store.select(selectCurrentUser);
+
+  
+
+  ngOnInit() {
+    // Load user details when component initializes
+    this.userId$.pipe(take(1)).subscribe((id) => {
+      if (id) {
+        this.store.dispatch(UserActions.selectUser({ id }));
+      }
+    });
+  }
+
+  goBack() {
+    this.router.navigate(['/users']);
+  }
 }
