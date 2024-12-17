6ea4cb56deaac739a62d5d00d809069cb0135412
harry log <harry8log@gmail.com>
2024-12-17 08:42:43 +0200

    update

--- ng-stamina/src/app/users/components/user-form/user-form.component.ts
+++ ng-stamina/src/app/users/components/user-form/user-form.component.ts
@@ -75,7 +75,7 @@ export class UserFormComponent implements OnInit {
     name: ['', [Validators.required, Validators.minLength(3)]],
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(4)]],
-    roles: [[] as UserRole[], [Validators.required]],
+    roles: [[UserRole.USER] as UserRole[], [Validators.required]],
   }) as FormGroup<UserFormModel>;
 
   ngOnInit() {
